454b374153dc196c7fbf4a93c2ff2e16f33078d9
Ian DeGroot <ianpdegroot@gmail.com>
2018-11-03 16:36:52 -0500

    Cleaning up the code.

--- pygame_timer.py
+++ pygame_timer.py
@@ -14,7 +14,9 @@ BLUE = (0, 0, 255)
 
 SECS_IN_MIN = 60
 
-# set up pygame
+TOP_RIGHT_COORDS = (0, 0)
+
+# Set up pygame
 pygame.init()
 
 # Timer class that will contain everything needed to create a countdown timer
@@ -24,58 +26,60 @@ class Timer:
         self.width = 230
         self.height = 50
         self.display = pygame.display.set_mode((self.width, self.height))
-        self.counter_value = secs
+        self.init_counter_value = secs
         self.count_font = pygame.font.SysFont("couriernew", 48)
         self.background_color = BLACK
+        self.start_ticks = None
+        self.in_count_mode = True
 
+    def update_time(self):
+        self.update_display(self.calculate_new_time())
+        
+    def calculate_new_time(self):
+        seconds = self.init_counter_value - ((pygame.time.get_ticks() - self.start_ticks) / 1000)
+        seconds = max(0, seconds)
+        if seconds == 0:
+            self.background_color = RED
+            self.in_count_mode = False
+        minutes, seconds = divmod(seconds, SECS_IN_MIN)
+        return self.count_font.render("{:02.0f}:{:05.2f}".format(minutes, seconds), True, WHITE)
 
+    def update_display(self, new_text):
+        timer.display.fill(timer.background_color)
+        timer.display.blit(new_text, TOP_RIGHT_COORDS)
+
+    def close(self):
+        pygame.quit()
+        sys.exit()
+
+# Window initialization
 clock = pygame.time.Clock()
 pygame.display.set_caption("Countdown Timer")
-icon = pygame.image.load("hourglass2.png")
+icon = pygame.image.load("resources/images/hourglass2.png")
 pygame.display.set_icon(icon)
 
-def close_timer():
-    pygame.quit()
-    sys.exit()
-
-start_ticks = pygame.time.get_ticks()
-
+# Create a Timer
 timer = Timer(int(sys.argv[1]))
 
 # Set up the text
-text = timer.count_font.render(str(timer.counter_value), True, WHITE)
-textRect = text.get_rect()
-textRect.centerx = timer.display.get_rect().centerx
-textRect.centery = timer.display.get_rect().centery
+text = timer.count_font.render(str(timer.init_counter_value), True, WHITE)
 
-# draw the white background onto the surface
-timer.display.fill(timer.background_color)
+# Get a reference number of ticks to start the timer
+timer.start_ticks = pygame.time.get_ticks()
 
-# draw the text onto the surface
-timer.display.blit(text, (0, 0))
-
-# draw the window onto the screen
-pygame.display.update()
-
-# run the game loop
+# Run the game loop
 while True:
+    # Check for events
     for event in pygame.event.get():
         if event.type == QUIT:
-            close_timer()
+            timer.close()
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
-                close_timer()
+                timer.close()
 
-    seconds = timer.counter_value - ((pygame.time.get_ticks() - start_ticks) / 1000)
-    seconds = max(0, seconds)
-    if seconds == 0:
-        timer.background_color = RED
-    minutes, seconds = divmod(seconds, SECS_IN_MIN)
-    text = timer.count_font.render("{:02.0f}:{:05.2f}".format(minutes, seconds), True, WHITE)
-
-    # Fill the screen
-    timer.display.fill(timer.background_color)
-    timer.display.blit(text, (0, 0))
+    # Check the mode and act accordingly
+    if timer.in_count_mode:
+        timer.update_time()
 
     # Refresh the screen
     pygame.display.flip()
