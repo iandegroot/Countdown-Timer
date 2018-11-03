b8b3339bcd0819835370165eef77d5d3161e7d8e
Ian DeGroot <ianpdegroot@gmail.com>
2018-11-03 17:54:18 -0500

    Adding pause/resume functionality.

--- pygame_timer.py
+++ pygame_timer.py
@@ -26,27 +26,37 @@ class Timer:
         self.width = 230
         self.height = 50
         self.display = pygame.display.set_mode((self.width, self.height))
-        self.init_counter_value = secs
+        self.init_counter_value_in_secs = secs
+        self.counter_value_in_secs = self.init_counter_value_in_secs
         self.count_font = pygame.font.SysFont("couriernew", 48)
         self.background_color = BLACK
-        self.start_ticks = None
-        self.in_count_mode = True
+        self.start_ticks = 0
+        self.in_count_mode = False
 
     def update_time(self):
         self.update_display(self.calculate_new_time())
-        
+
     def calculate_new_time(self):
-        seconds = self.init_counter_value - ((pygame.time.get_ticks() - self.start_ticks) / 1000)
-        seconds = max(0, seconds)
-        if seconds == 0:
+        secs_passed = ((pygame.time.get_ticks() - self.start_ticks) / 1000)
+        self.counter_value_in_secs = self.init_counter_value_in_secs - secs_passed
+        self.counter_value_in_secs = max(0, self.counter_value_in_secs)
+        if self.counter_value_in_secs == 0:
             self.background_color = RED
             self.in_count_mode = False
-        minutes, seconds = divmod(seconds, SECS_IN_MIN)
+        minutes, seconds = divmod(self.counter_value_in_secs, SECS_IN_MIN)
         return self.count_font.render("{:02.0f}:{:05.2f}".format(minutes, seconds), True, WHITE)
 
     def update_display(self, new_text):
-        timer.display.fill(timer.background_color)
-        timer.display.blit(new_text, TOP_RIGHT_COORDS)
+        self.display.fill(self.background_color)
+        self.display.blit(new_text, TOP_RIGHT_COORDS)
+
+    def toggle_mode(self):
+        self.in_count_mode = not self.in_count_mode
+        if self.in_count_mode:
+            # Get a reference number of ticks to start the timer
+            self.start_ticks = pygame.time.get_ticks()
+        else:
+            self.init_counter_value_in_secs = self.counter_value_in_secs
 
     def close(self):
         pygame.quit()
@@ -62,10 +72,9 @@ pygame.display.set_icon(icon)
 timer = Timer(int(sys.argv[1]))
 
 # Set up the text
-text = timer.count_font.render(str(timer.init_counter_value), True, WHITE)
-
-# Get a reference number of ticks to start the timer
-timer.start_ticks = pygame.time.get_ticks()
+minutes, seconds = divmod(timer.init_counter_value_in_secs, SECS_IN_MIN)
+text = timer.count_font.render("{:02.0f}:{:05.2f}".format(minutes, seconds), True, WHITE)
+timer.display.blit(text, TOP_RIGHT_COORDS)
 
 # Run the game loop
 while True:
@@ -76,6 +85,9 @@ while True:
         elif event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 timer.close()
+        elif event.type == MOUSEBUTTONDOWN:
+            if timer.counter_value_in_secs != 0:
+                timer.toggle_mode()
 
     # Check the mode and act accordingly
     if timer.in_count_mode:
